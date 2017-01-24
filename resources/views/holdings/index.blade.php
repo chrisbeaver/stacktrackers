cc1c38c725168693b653b4b3af3aef8ff836512c
Chris Beaver <chris@chrisbeaver.net>
2017-01-24 14:20:19 -0500

    Add new holdings button and routes.

--- resources/views/holdings/index.blade.php
+++ resources/views/holdings/index.blade.php
@@ -2,7 +2,16 @@
 
 @section('main-content')
 <div class="content">
-    
+    <div class="container" style="margin-top: 2em;">
+        <div class="row">
+            <a class="button is-medium is-info" href="{{ action('HoldingController@showNewForm') }}">
+                <span class="icon is-medium">
+                    <i class="fa fa-plus"></i>
+                </span>
+                <span>Add Piece</span>
+            </a>
+        </div>
+    </div>
     <holdings></holdings>
 
 </div>
@@ -11,7 +20,7 @@
 @section('footer-assets')
 <script>
     const app = new Vue({
-    el: '.content'
-});
+        el: '.content'
+    });
 </script>
 @endsection
