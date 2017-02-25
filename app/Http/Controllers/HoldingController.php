c53d5f3aba0e6d7cfb404901cee5a7900918413c
Chris Beaver <chris@chrisbeaver.net>
2017-02-25 14:47:49 -0500

    My holdings index page.

--- app/Http/Controllers/HoldingController.php
+++ app/Http/Controllers/HoldingController.php
@@ -13,8 +13,11 @@ class HoldingController extends Controller
     public function showMyHoldings()
     {
         $holdings = auth()->user()->holdings;
+        $total_pieces = $holdings->sum('quantity');
+        $total_weight = $holdings->sum('weight');
+        $tags = ['ASE', 'Maple Leaf', 'Panda'];
         // return dd($holdings);
-        return view('holdings.index', compact('holdings'));
+        return view('holdings.index', compact('holdings', 'total_pieces', 'total_weight', 'tags'));
     }
 
     public function showHolding($id)
@@ -31,15 +34,15 @@ class HoldingController extends Controller
     public function store(HoldingRequest $request)
     {
         $date = Carbon::createFromFormat('m-d-Y', $request->purchase_date)->format('Y-m-d');
-        $holding = Holding::create(['name' => $request->name, 'weight' => $request->weight, 
-            'weight_unit' => $request->weight_unit, 'quantity' => $request->quantity, 
-            'finess' => $request->finess, 'purchase_price' => $request->purchase_price * 100, 
+        $holding = Holding::create(['name' => $request->name, 'weight' => $request->weight,
+            'weight_unit' => $request->weight_unit, 'quantity' => $request->quantity,
+            'finess' => $request->finess, 'purchase_price' => $request->purchase_price * 100,
             'purchase_date' => $date, 'user_id' => $request->user_id,
             'purchase_currency' => $request->purchase_currency]);
         // Store holding in session to attach images to it.
         session()->flash('active_holding', $holding->id);
 
-        return redirect()->action('ImageController@addImagesForm'); 
+        return redirect()->action('ImageController@addImagesForm');
 
     }
 }
--- resources/views/holdings/profile.blade.php
+++ resources/views/holdings/profile.blade.php
@@ -5,9 +5,50 @@
 @endpush
 
 @section('main-content')
-	@foreach($holding->images as $image)
-	<a class="venobox" data-gall="holdings" href="{{ action('ImageController@showImage', ['user_id' => $holding->user_id, 'image_id' => $image->id]) }}"><img src="{{ action('ImageController@showThumb', ['user_id' => $holding->user_id, 'image_id' => $image->id]) }}" alt="image alt"/></a>
+<div class="container">
+    <div class="columns">
+        <div class="column is-one-third">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image" style="height: 40px; width: 40px;">
+                                <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                        <a>#css</a> <a>#responsive</a>
+                        <br />
+                        <small>11:09 PM - 1 Jan 2016</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-two-thirds">
+
+            <div class="container">
+                <h1 class="title">{{ $holding->name }}</h1>
+                <h2 class="subtitle">Hero subtitle</h2>
+            </div>
+
+        </div>
+    </div>
+    @foreach($holding->images as $image)
+	   <a class="venobox" data-gall="holdings" href="{{ action('ImageController@showImage', ['user_id' => $holding->user_id, 'image_id' => $image->id]) }}"><img src="{{ action('ImageController@showThumb', ['user_id' => $holding->user_id, 'image_id' => $image->id]) }}" alt="image alt"/></a>
 	@endforeach
+</div>
 @endsection
 
 @push('scripts')
